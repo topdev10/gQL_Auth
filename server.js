ded9b34484388c916426664427d689a123f89413
hiddenhelper <bountyhunter0308@hotmail.com>
2020-04-10 06:11:46 -0700

    connecting postgresDB using sequelize

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const bodyParser = require('body-parser')
 
 const app = express();
 
+// Listen-------------------
 app.listen(3000, function() {
     console.log('listening on 3000')
 })
 
+// -------****-------------------
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// Routes---
+// Routes-------------------
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
